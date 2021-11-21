747ca2821b4f3ebd6109a92b097b106fdcdfb006
Elnatan Torres | Stone <etorres@stone.com.br>
2021-11-21 15:09:08 -0300

    Create new AP

---
+++ SchoolGrades.API/Controllers/AssessmentGradeController.cs
@@ -0,0 +1,61 @@
+﻿using Google.Cloud.Firestore;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SchoolGrades.API.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SchoolGrades.API.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/assessment-grades")]
+    [ApiVersion("1.0")]
+    public class AssessmentGradeController : ControllerBase
+    {
+        private readonly FirestoreDb _firestoreDb;
+        private string _credentialsDirectory = Directory.GetCurrentDirectory() + "\\usjt-a3-332601-a3c49cdf4250.json";
+
+        public AssessmentGradeController()
+        {
+            
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", _credentialsDirectory);
+            _firestoreDb = FirestoreDb.Create("usjt-a3-332601");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            Query gradeQuery = _firestoreDb.Collection("grade");
+            QuerySnapshot snapshot = await gradeQuery.GetSnapshotAsync();
+
+            List<AssessmentGrade> grades = new List<AssessmentGrade>();
+
+            foreach(DocumentSnapshot documentSnapshot in snapshot.Documents)
+            {
+                if(documentSnapshot.Exists)
+                {
+                    Dictionary<string, object> gradesDict = documentSnapshot.ToDictionary();
+                    string json = JsonConvert.SerializeObject(gradesDict);
+
+                    AssessmentGrade assessmentGrade = JsonConvert.DeserializeObject<AssessmentGrade>(json);
+                    assessmentGrade.AssessmentId = documentSnapshot.Id;
+
+                    grades.Add(assessmentGrade);
+                }
+            }
+
+            return Ok(grades);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(AssessmentGrade grade)
+        {
+            CollectionReference collectionReference = _firestoreDb.Collection("grade");
+            await collectionReference.AddAsync(grade);
+
+            return Created("", "");
+        }
+    }
+}
---
+++ SchoolGrades.API/Models/AssessmentGrade.cs
@@ -0,0 +1,54 @@
+﻿using Google.Cloud.Firestore;
+using System;
+
+namespace SchoolGrades.API.Models
+{
+    [FirestoreData]
+    public class AssessmentGrade
+    {
+        /// <summary>
+        /// The assessment id.
+        /// </summary>
+        public string AssessmentId { get; set; }
+        /// <summary>
+        /// The evaluated assessment.
+        /// </summary>
+        [FirestoreProperty]
+        public string Assessment { get; set; }
+
+        /// <summary>
+        /// The assessment date.
+        /// </summary>
+        [FirestoreProperty]
+        public string AssessmentDate { get; set; }
+
+        /// <summary>
+        /// The grade of the assessment.
+        /// </summary>
+        [FirestoreProperty]
+        public float Grade { get; set; }
+
+        /// <summary>
+        /// The date which the grade was given.
+        /// </summary>
+        [FirestoreProperty]
+        public string GradeDate { get; set; }
+
+        /// <summary>
+        /// The name of the evaluated student.
+        [FirestoreProperty]
+        public string StudentName { get; set; }
+
+        /// <summary>
+        /// The evaluated subject.
+        /// </summary>
+        [FirestoreProperty]
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// The teacher who performed the evaluation.
+        /// </summary>
+        [FirestoreProperty]
+        public string Teacher { get; set; }
+    }
+}
